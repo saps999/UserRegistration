f7a99fd0906e56e3fe33518c1a6d174fd7b7e7a6
saps999 <saptarshibiswasgang@gmail.com>
2022-12-08 21:04:12 +0530

    Validating First Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -7,5 +7,10 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration: ");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First name: ");
+        boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
+        System.out.println("Result : " + firstName);
     }
 }
