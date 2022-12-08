961b86456ce423de7f71d6c2a2275f68c80ce864
saps999 <saptarshibiswasgang@gmail.com>
2022-12-08 21:21:39 +0530

    Validating Phone Number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -8,20 +8,38 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration: ");
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter First name: ");
+        Scanner scanner =new Scanner(System.in);
+        /*
+        First name starts with Cap and has minimum 3 characters
+         */
+        System.out.print("Enter First name: ");
         boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + firstName);
 
         System.out.println(" ");
-
+        /*
+        Last name starts with Cap and has minimum 3 characters
+         */
         System.out.print("Enter last name: ");
         boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + lastName);
 
+        System.out.println(" ");
+        /*
+         Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+         */
         System.out.print("Enter your e-mail id: ");
-        boolean email = Pattern.matches("[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
+        boolean email = Pattern.matches("[a-zA-Z0-9.]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
         System.out.println("Result : " + email);
+
+        System.out.println(" ");
+
+        /*
+         Country code follow by space and 10 digit number
+         */
+        System.out.print("Enter your Mobile number: ");
+        boolean mobileNo = Pattern.matches("[0-9]{1,15}\s[9876]{1}[0-9]{9}",scanner.nextLine());
+        System.out.println("Result : " + mobileNo);
 
         System.out.println(" ");
     }
